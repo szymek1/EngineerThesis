df1d0ba962e7db36355f8afa21fbfccad7aaea1a
szymek1 <bogusszymon2000@gmail.com>
2023-04-25 00:24:58 +0200

    Mean Shift added, bugs to be fixed

--- preprocessing/prepare_images.py
+++ preprocessing/prepare_images.py
@@ -5,7 +5,7 @@ Script activating processing pipeline
 import argparse
 from typing import Tuple
 
-from utils.preprocess import Preprocessor
+from utils.preprocess import Preprocessor, MSProcessor
 
 
 def tuple_of_ints(s) -> Tuple[int]:
@@ -19,13 +19,26 @@ def main(my_args: argparse.Namespace) -> None:
     imgs_directories = my_args.dirs
     save_destination = my_args.dest
     new_resolution = my_args.res
+    use_ms = my_args.ms_use
 
     processor = Preprocessor(imgs_directories, save_destination)
 
+    # TODO: make more clear !!!
     if new_resolution is not None:
-        processor.change_resolution(new_resolution)
+        if use_ms is True:
+            print("Using mean shift processor")
+            processor = MSProcessor(imgs_directories, save_destination, 0.06, 100)
+            processor.ms_cluster(False, new_resolution)
+        else:
+            processor.change_resolution(new_resolution)
     else:
-        print("No resolution specified, default resolution will be used")
+        print("No resolution specified, default resolution will be used\nUsing mean shift processor")
+        if use_ms is True:
+            processor = MSProcessor(imgs_directories, save_destination, 0.01, 100)
+            processor.ms_cluster(True)
+        else:
+            print("No action to take")
+            exit()
 
 
 if __name__ == "__main__":
@@ -34,6 +47,7 @@ if __name__ == "__main__":
     parser.add_argument("--dirs", metavar='dir', help="paths to images directories", nargs='+')
     parser.add_argument("--dest", type=str, help="destination of processed images to be saved to")
     parser.add_argument("--res", metavar='tuple', type=tuple_of_ints, help="set new resolution", nargs='?')
+    parser.add_argument("--ms_use", type=bool, help="cluster images using Mean Shift", nargs='?')
     parser.add_argument("-h", "--help", action="help", help="Display this message")
 
     args = parser.parse_args()
